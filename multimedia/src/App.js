99942fb32a22f7a7bbbd562ad29bf4e24c3fe4e5
Yaar-Nuul <yaarnuulreech@gmail.com>
2024-07-13 21:31:44 +0300

    add navbar/landingpage

--- multimedia/src/App.js
+++ multimedia/src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import Navbar from './Navbar/index.js'
+import LandingPage from './Landing-page/Index.js'
 
 function App() {
   return (
     <div className="App">
-    
+    <Navbar/>
+    <LandingPage/>
     </div>
   );
 }
