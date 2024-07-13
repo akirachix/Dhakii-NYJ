07cb1de88b09462e18bcd6b6d3de7ec6a201158b
shallo-nairat <shallonairat@gmail.com>
2024-07-13 23:55:55 +0300

    new files

--- multimedia/src/App.js
+++ multimedia/src/App.js
@@ -1,10 +1,10 @@
+import Portfolio from "./Portfolio";
 
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    
+    <div>
+    <Portfolio/>
     </div>
   );
 }
---
+++ multimedia/src/Portfolio/index.js
@@ -0,0 +1,464 @@
+
+
+import './index.css'
+import Nay from '../Images/Nay.png'
+import Yar from '../Images/Yar.png'
+import Bridget from '../Images/Bridget.png'
+import Casey from '../Images/Casey.png'
+import Martha from '../Images/Martha.png'
+ function Portfolio(){
+    return(
+ <div className='Portfolios'>
+  <div>
+        <h3 id='portfolio'>Our Portfolios</h3>
+      </div>
+
+  <div className='Nairat'>
+      
+      <div id='Nairat'>
+
+        <div className='About'>
+        <div>
+        <img src= {Nay} alt='photo' id='nairat'/>
+      </div>
+      <div>
+        <h3 id='name'>NAIRAT  SHALLO</h3>
+
+        <h3 id='ps'>Personal Statement</h3>
+
+
+        <div id='personal-statement'>
+
+        <p> +254769333487 | Linked In | Github
+        </p>
+        The uneven classification of learning outcomes has resulted in detrimental effects.
+        Specifically, in Kilifi County, schools in rural areas suffer from severe understaffing 
+        compared to their urban counterparts. Nairat, an ardent education enthusiast and passionate
+        software engineer who firmly believes in the transformative power of education for personal 
+        growth and societal progress, is driven by her belief in the power of technology to revolutionize 
+        education. Recognizing its crucial role in fostering equitable learning opportunities for all, she 
+        is particularly passionate about leveraging technology to establish accessible, engaging, and inclusive 
+        educational environments that cater to the needs of young people across diverse backgrounds.
+
+        <h4 id='education'>EDUCATION</h4>
+        <p >AkiraChix 
+           codeHive - Diploma in Information Technology 
+           February 2024- Present
+        </p>
+
+        </div>
+      </div>
+      </div>
+
+        <h3 id='myprofile'>MY PROFILE</h3>
+
+      <div className='myprofile'>
+      
+         
+
+      <div id='skills'>
+      <h4 >SKILLS</h4>
+
+        <p >
+        Well-versed with HTML, CSS, and JavaScript to create functional web applications.
+        Ability to dynamically alter content, structure, and style within web applications using the Document Object Mode(DOM), ensuring responsive and interactive user interfaces.
+        Ability to implement CSS media queries for creating responsive designs that adapt to various screen sizes and devices, ensuring a consistent user experience across platform
+        Skilled in interacting with RESTful APIs to fetch, update, and manage data within web applications.
+
+        </p>
+      </div>
+      <div id='skills' >
+         <h3>Relevant Courses </h3>
+         <p >
+         Front-end Development
+
+         Fetching Restful APIs.
+         Creating web apps using HTML, CSS, and React.js framework.
+         Manipulating the Document Object Model (DOM) in JavaScript.
+
+         Product Management
+
+         Testing and validating a product
+         Familiarity with Agile Principles
+         Managing Product Lifecycle and Development Processes
+        
+
+         Data and Software Architecture
+
+         Data Analysis 
+         Knowledge of SQL Database.
+         Creating Relationship Diagrams.
+         Methods for Quantitative and Qualitative Analysis
+         A clear understanding of data structures and algorithms.
+
+
+         </p>
+      </div>
+      </div>
+
+    </div>
+    </div>
+    <br></br>
+    <div className='Yar'>
+      
+      <div id='Yar'>
+
+        <div className='About'>
+        <div>
+        <img src= {Yar} alt='photo' id='nairat'/>
+      </div>
+      <div>
+        <h3 id='name'>YAR NUUL</h3>
+
+        <h3 id='ps'>Personal Statement</h3>
+
+        <div id='personal-statement'>
+
+
+        <p> +254769333487 | Linked In | Github
+        </p>
+
+        <p>
+        Escaping the limitations of Kenya's refugee camp education system,
+        Yar is on a mission to bridge the educational gap. Using her software
+        skills, she aims to dismantle the barriers that block refugee children 
+        from accessing the resources they need to thrive. Through innovative technology,
+        Yar envisions inclusive learning environments that nurture the academic and 
+        personal growth of all, regardless of background.
+        <h4 id='education'>EDUCATION</h4>
+
+        <p>AkiraChix 
+           codeHive - Diploma in Information Technology 
+           February 2024- Present
+        </p>
+        </p>
+        </div>
+      </div>
+      </div>
+      <div>
+      
+x      <h3 id='myprofile'>MY PROFILE</h3>
+
+       <div className='myprofile'>
+
+      <div id='skills'>
+         
+         
+        <h4>SKILLS</h4>
+        <p>
+          
+        Strong understanding of HTML, CSS, and JavaScript
+        Skilled in web development using JavaScript, React, and HTML/CSS
+        Familiarity with JavaScript  libraries such as React
+        Proficient in developing responsive user interfaces for various devices and browsers
+
+        </p>
+      </div>
+      <div id='skills' >
+         <h3>Relevant Courses </h3>
+         <p>
+         UX Research: 
+         The UX Research course emphasizes the importance of understanding user needs,
+         behaviors, and preferences through various research methodologies. The course
+         is centered around topics such as:Qualitative and Quantitative Research Techniques
+         Usability Testing and User Feedback Gathering
+
+
+         Product Management: 
+         It majorly centered on  the oversight of the entire product lifecycle, from its 
+         beginning to its end.Key concepts covered in this course include:
+         Product Strategy and User Journey mapping and User Research and Feedback Integration
+
+
+         
+         The Mobile Development course focuses on developing mobile applications, with a 
+         specific emphasis on the Kotlin programming language and the use of Android Studio.
+         Key topics include:Kotlin Programming Language Android Studio Development Environment
+         And Mobile App Architecture and Design Patterns
+
+
+
+
+
+         </p>
+         </div>
+      </div>
+      </div>
+    </div>
+    </div>
+
+    <div className='Bridget'>
+      
+      <div id='Bridget'>
+
+        <div className='About'>
+        <div>
+        <img src= {Bridget} alt='photo' id='nairat'/>
+      </div>
+      <div>
+        <h3 id='name'>BRIDGET KATHURE</h3>
+        <h3 id='ps'>Personal Statement</h3>
+
+        <div id='personal-statement'>
+
+        <p> +254769333487 | Linked In | Github
+        </p>
+
+        <p>
+        Bridget witnessed the delicate balance between human activities and precious
+        ecosystems in wildlife conservation areas from an early age. This sparked a
+        deep sense of responsibility for wildlife and their habitats. She is excited
+        by the application of emerging technologies such as remote sensing and GIS 
+        guide effective conservation efforts. As a software developer, she plans 
+        to leverage her software development skills to empower local communities 
+        by providing them with ecological data and knowledge to protect the land 
+        and animals that live in it.
+        <h4>EDUCATION</h4>
+
+        <p>AkiraChix 
+           codeHive - Diploma in Information Technology 
+           February 2024- Present
+        </p>
+        </p>
+        </div>
+      </div>
+      </div>
+      <div>
+      
+    <h3 id='myprofile' >MY PROFILE</h3>
+
+    <div className='myprofile'>
+
+     <div id='skills'>
+
+      <h4>SKILLS</h4>
+
+
+        <p>
+          
+        Well-versed with Javascript and RESTful APIs.
+        Capability to build RESTful APIs using the Django framework.
+        Skilled in DOM manipulation for dynamic web page interactions.
+        Ability to use Kotlin’s recyclerView and CardViews to compute data.
+
+
+        </p>
+      </div>
+      <div id='skills'>
+
+         <h3>Relevant Courses </h3>
+         <p>
+         Backend Development
+
+         Familiar with backend web frameworks like Django. 
+         Object-oriented programming, creating classes and objects.
+         Ability to create backend applications using the Django framework.
+
+
+         Frontend Web Development
+
+         Building web applications using React.
+         Ability to create responsive websites.
+         Ability to use DOM manipulation in JavaScript.
+
+
+         Data and Software Architecture
+
+         Ability to use PostgreSQL.
+         Problem-solving techniques.
+         Algorithmic problem solving.
+
+
+
+
+         </p>
+         </div>
+      </div>
+      </div>
+    </div>
+    </div>
+
+     
+    <div className='Casey'>
+      
+      <div id='Casey'>
+
+        <div className='About'>
+        <div>
+        <img src= {Casey} alt='photo' id='nairat'/>
+      </div>
+      <div>
+        <h3 id='name'>CASEY OCHIENG</h3>
+
+        <p>
+
+        <h3 id='ps'>Personal Statement</h3>
+
+        </p>
+        <div id='personal-statement'>
+
+
+        <p >
+        <p>minuochieng@gmail.com | Github | Linkedin | +254741151470</p>
+
+        A cinematic vision that skillfully crafts emotions communicates the 
+        core of the story while simultaneously providing a visual feast. 
+        From the era of silent black and white films to the stunning works
+        of high definition, technology has changed storytelling and enhanced 
+        visual experiences in equal measure. Casey is eager to show how authentic 
+        storytelling and innovative editing techniques can turn everyday events 
+        into remarkable stories that connect with the audience. She is excited 
+        to see how cutting-edge technologies can expand the possibilities for 
+        visual storytelling to contribute to this exciting and ever-evolving 
+        profession. As a software developer, Casey aims to use 3D previsualization
+        to make cinematic experiences that are compelling and impactful, appealing
+        to a global audience.
+        <h4>EDUCATION</h4>
+        <p>AkiraChix 
+           codeHive - Diploma in Information Technology 
+           February 2024- Present
+        </p>
+
+        </p>
+        </div>
+      </div>
+      </div>
+      <div>
+      
+x      <h3 id='myprofile'>MY PROFILE</h3>
+
+      <div>
+      <div className='myprofile'>
+      <div id='skills'>
+
+
+        <h4>SKILLS</h4>
+        <p>
+          
+        Knowledge of Javascript, HTML, and  CSS to develop dynamic and responsive web applications.
+        Knowledge of SQL database operations, including data management, querying, and database integrity efficient
+        Knowledge in creating database schemas that represent and support business processes, integrating multiple data sources and databases into one system.
+        Capable of using Android Studio IDE to develop feature-rich mobile applications for Android devices.
+
+        </p>
+      </div>
+      <div id='skills'>
+         <h3 >Relevant Courses </h3>
+         <p>
+         Backend Development
+         Basic data types(Defining variables, Integers, and floats)
+         Introduction to web development using  Django(creating a virtual environment.)
+
+         Frontend Development
+         Introduction to HTML, CSS
+         Control Flows. (For Loops, Data types, Operators, and Bitwise Operators)
+
+
+         Mobile Development
+         Functions(Parameter and return types)
+         Flow controlInheritance and Abstract classes
+
+
+         Data and Software Architecture
+         Problem-solving technics.
+         Introduction to methods of data collection.
+
+
+
+
+         </p>
+         </div>
+         </div>
+      </div>
+      </div>
+    </div>
+    </div>
+
+    <div className='Martha'>
+      
+      <div id='Martha'>
+
+        <div className='About'>
+        <div>
+        <img src= {Martha} alt='photo' id='nairat'/>
+      </div>
+      <div>
+        <h3 id='name'>MARTHA MUTUA</h3>
+
+      
+        <h3 id='ps'>Personal Statement</h3>
+
+        <p id='personal-statement'>
+        <p> Tel no: +254719265993 |Linkedin |Github |Email
+        </p>
+        Martha comes from a community where a quarter of the population suffers
+        from cancer and struggles to access proper care. Shockingly, most of 
+        the hospitals in the area lack the crucial diagnostic tools needed to
+        tackle this disease effectively. After the loss of her grandmother due
+        to a misdiagnosed illness, Martha witnessed firsthand the harsh realities
+        of rural healthcare. This experience ignited a fierce determination within 
+        her to pursue software engineering, with a firm belief in the power of technology 
+        to address these pressing issues. She intends to leverage her software development 
+        skills in machine learning and artificial intelligence to develop user-friendly and 
+        easily accessible diagnostic tools, such as rapid scanners and diagnostic devices, 
+        to mitigate inadequate medical services.
+        <h4>EDUCATION</h4>
+        <p>AkiraChix 
+           codeHive - Diploma in Information Technology 
+           February 2024- Present
+        </p>
+       </p>
+      </div>
+      </div>
+      <div>
+      
+           <h3 id='myprofile'>MY PROFILE</h3>
+
+           <div className='myprofile'>
+      <div id='skills'>
+        
+        
+        <h4>SKILLS</h4>
+        <p>
+          
+        Ability to use algorithms and pseudocodes to solve problems and develop new solutions.
+        Familiarity with using SQL queries and subqueries to interact with relational databases.
+        Ability to create CTEs and Case Statements to make code readable and easy to debug.
+        Ability to prioritize tasks and focus effort on the most important and impactful projects.
+       </p>
+      </div>
+      <div id='skills'>
+         <h3>Relevant Courses </h3>
+         <p>
+         Frontend Development
+         HTML/CSS /Javascript
+         DOM/APIs
+         React.Js
+
+         Mobile Development
+         Kotlin 
+         Android Studio
+         RecyclerViews
+
+         Backend Development
+         Linux Basics
+         Data types/Classes and OOP
+         Django projects
+
+
+
+
+         </p>
+         </div>
+         
+      </div>
+      </div>
+    </div>
+    </div>
+  </div>
+
+    )
+   
+ }
+
+ export default Portfolio;
